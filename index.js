1833c7f7960aafe8dd0ed8697dd34dd2668a6f12
BranimirTomeljak <mtomeljak@gmail.com>
2023-11-01 20:25:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use("/", require("./routes/main"));
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
